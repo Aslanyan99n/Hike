aef36ffa3c2618b50dbd788672bb26ea7bba3b89
Aslanyan99n <a99narek@gmail.com>
2023-06-14 09:34:35 +0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Narek on 14.06.23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
